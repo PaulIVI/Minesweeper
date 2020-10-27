d977e5e73bd450d1aba838c7124f6468c89124e4
PaulIVI <mailformachines01@gmail.com>
2020-10-27 18:54:22 +0100

    Revert "Merge remote-tracking branch 'origin/dev' into feature/openAField"
    This reverts commit c69530c21c01ce085fd93f84641c7936ca6a7ad2, reversing
    changes made to 51eff7e133fc3e15b9e246216bec50a9f20d5e1a.

--- src/main/Minesweeper.scala
+++ src/main/Minesweeper.scala
@@ -1,40 +1,21 @@
-import main.controller.Controller
-import main.model.{GameLogic, GameMap}
-import main.view.Tui
-
-import scala.io.StdIn.readLine
-
 object Minesweeper {
   def main(args: Array[String]) = {
-    val controller = new Controller(new GameMap(10), new GameLogic)
-    val tui = new Tui(controller)
-    controller.notifyObservers
+    val difficulty = 10
 
-    def main(args: Array[String]): Unit = {
-      var input: String = "r"
-      if (!input.isEmpty) tui.processInputLine(input)
-      else do {
-        input = readLine()
-        tui.processInputLine(input)
-      } while (input != "q")
-    }
+    val game_map = Vector(Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0))
+    val current_game_map = Vector(Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1))
 
-//    val difficulty = 10
-//
-//    val game_map = Vector(Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0), Vector(1,0,0,0,1,0))
-//    val current_game_map = Vector(Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1), Vector(0,1,2,3,0,1))
-//
-//    val user_interface = new main.model.UI
-//    val console_string = user_interface.gameMapToString(game_map, current_game_map)
-//    println(console_string)
-//
-//    val mine = new main.model.GameMap
-//    val mapMine = mine.createMap(difficulty)
+    val user_interface = new UI
+    val console_string = user_interface.gameMapToString(game_map, current_game_map)
+    println(console_string)
 
-//    val logic = new GameLogic
-//    val game_situation = logic.startTheGame(difficulty)
-//
-//    printf(mapMine.toString())
-//    printf(game_situation.toString())
+    val mine = new GameMap
+    val mapMine = mine.create_map(difficulty)
+
+    val logic = new GameLogic
+    val game_situation = logic.start_the_game(difficulty)
+
+    printf(mapMine.toString())
+    printf(game_situation.toString())
   }
 }
--- src/main/UI.scala
+++ src/main/UI.scala
@@ -1,6 +1,4 @@
-package main.model
-
-class GameMapString{
+class UI{
   def gameMapToString(game_map:Vector[Vector[Int]], current_game_map:Vector[Vector[Int]]):String ={
     var result = generateFirstRow(game_map)
 

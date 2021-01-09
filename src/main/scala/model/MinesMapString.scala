f683bcdfd555fdf6b1552cce365454209e6b683e
tillreitlinger <Till-Reitlinger@outlook.de>
2021-01-09 12:13:08 +0100

    Added some small new language features.

--- src/main/scala/model/MinesMapString.scala
+++ src/main/scala/model/MinesMapString.scala
@@ -1,6 +1,6 @@
 package main.model
 
-class MinesMapString{
+class MinesMapString:
   def gameMapToString(game_map:Vector[Vector[Int]], current_game_map:Vector[Vector[Int]]):String =
     val first_row = generateFirstRow(game_map)
     val rest_of_grid = generateRestOfGrid(game_map, current_game_map)
@@ -16,23 +16,23 @@ class MinesMapString{
     current_game_map_string
   
   def addVectorElementToString(value:Int, row:Vector[Int], row_index:Int, column_index:Int, game_map:Vector[Vector[Int]]): String =
-    val result1 = if (column_index == 0) row_index + " " else ""
+    val result1 = if (column_index == 0) s"${row_index} " else ""
     val result2 = result1 + returnVisibleChar(value, row_index, column_index, game_map)
     val result3 = if(column_index == row.length-1) result2 + "\n" else result2
     result3
+  
 
-  def generateFirstRow(game_map:Vector[Vector[Int]]): String =
+  def generateFirstRow(game_map:Vector[Vector[Int]]): String ={
     val abc = Vector("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
     val abc_correct_length = abc.slice(0, game_map.length)
     val first_row = "  " + abc_correct_length.mkString(" ") + " \n"
     first_row
-  
+  }
   def returnVisibleChar(value:Int, row_index:Int, column_index:Int, game_map:Vector[Vector[Int]]) =
     value match {
       case 0 => "# "
-      case 1 => game_map(row_index)(column_index) + " "
+      case 1 => s"${game_map(row_index)(column_index)} "
       case 2 => "! "
       case 3 => "? "
       case _ => "E "
     }
-}
